164ca5c94ee2681b80bae4968178f6a4a2f22eba
Curtis O <curtis@case.edu>
2023-07-08 10:11:07 -0400

    first commit

---
+++ new.py
@@ -0,0 +1,26 @@
+
+# notes on the venv and start up
+# mac:
+#     python3 -m venv env
+#     source ./env/bin/activate
+    
+# Windows: python -m venv env
+# .\env\Scripts\activate
+
+# Both:
+#     pip install requests
+#     pip freeze > requirements.txt
+
+# git init notes
+# git remote add origin https://github.com/CurtisONeal/requests_toy.git
+# git add .  
+# git commit -m 'Iinitial commit'
+# git push origin main/master
+
+# git push origin master
+
+# reminder for second commit.... if you just tried git push
+# git push --set-upstream origin master
+
+# starting up the app reminder to test for typos:
+# python3 app.py # to see the db created
